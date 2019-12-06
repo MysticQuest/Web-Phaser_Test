8b5f8a13947ac6c04d8d0a65591952cb90f546c2
thatguy <xarodasimba@gmail.com>
2019-12-06 18:04:43 +0200

    added background sound effects

--- game.js
+++ game.js
@@ -28,12 +28,25 @@ function preload() {
   this.load.plugin('DialogModalPlugin', './dialog.js');
   this.load.image("bg", "./assets/eforia.png");
   this.load.image("char", "./assets/bk.png");
+  this.load.audio("bg_audio","./assets/backgroundAudio.mp3");
 
 }
 
 function create() {
   this.add.image(700, 300, "bg");
   this.add.image(300, 300, "char");
+  this.bgAudio = this.sound.add("bg_audio");
+  var musicConfig = {
+    mute: false ,
+    volume: 0.7 ,
+    rate: 1 ,
+    detune: 0,
+    seek: 0,
+    loop: true,
+    delay: 0
+
+  };
+  this.bgAudio.play(musicConfig);
   this.sys.install('DialogModalPlugin');
   this.sys.dialogModal.init();
   this.sys.dialogModal.setText(pickLine(), true);
