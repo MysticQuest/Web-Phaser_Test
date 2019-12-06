cdb89d526fdc4f14e53524cdbafcffd72f597016
Mystic Quest <l1skou@gmail.com>
2019-12-06 02:23:11 +0200

    init and dialog plugin pt1

---
+++ game.js
@@ -0,0 +1,31 @@
+/* eslint-disable quotes */
+/* eslint-disable brace-style */
+/* eslint-disable space-before-function-paren */
+/* eslint-disable eol-last */
+/* eslint-disable no-unused-vars */
+/* eslint-disable indent */
+// The game config that is used by Phaser
+
+
+var config = {
+  type: Phaser.AUTO,
+  parent: 'phaser-example',
+  width: 800,
+  height: 600,
+  scene: {
+    preload: preload,
+    create: create
+  }
+};
+
+// Create a new Phaser Game object
+var game = new Phaser.Game(config);
+
+function preload() { this.load.plugin('DialogModalPlugin', './dialog.js'); this.load.image("bg", "./public/assets/bg.jpg"); }
+
+function create() {
+  this.add.image(700, 300, "bg");
+  this.sys.install('DialogModalPlugin');
+  this.sys.dialogModal.init();
+
+}
