8a67a78702fda31bfb1a67f2134dd312527eeb33
Mystic Quest <l1skou@gmail.com>
2019-12-06 04:30:21 +0200

    small refactor

--- textPicker.js
+++ textPicker.js
@@ -13,12 +13,12 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable indent */
 
+const greetArray = ["Hello, ", "Greetings, ", "Hey dawg, ", "Sup man, ", "I've been waiting in queue for hours and ", "Help me, "];
+const firstArray = ["my cat ", "my uncle ", "the neighbour ", "my ex-wife ", "my dog ", "my lawyer ", "a kid "];
+const secondArray = ["is pressing charges against me.", "died today...", "stole my car!", "has been deported!", "is talking shit about me!"];
 
 
 function pickLine() {
-    const greetArray = ["Hello, ", "Greetings, ", "Hey dawg, ", "Sup man, ", "I've been waiting in queue for hours and ", "Help me, "];
-    const firstArray = ["my cat ", "my uncle ", "the neighbour ", "my ex-wife ", "my dog ", "my lawyer ", "a kid "];
-    const secondArray = ["is pressing charges against me.", "died today...", "stole my car!", "has been deported!", "is talking shit about me!"];
     const greet = greetArray[Math.floor(Math.random() * greetArray.length)];
     const first = firstArray[Math.floor(Math.random() * firstArray.length)];
     const second = secondArray[Math.floor(Math.random() * secondArray.length)];
